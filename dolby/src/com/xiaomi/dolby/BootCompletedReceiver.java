0792e4aa7a741e789f29e3f5aad379fd796bca12
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-08-11 01:42:11 +0530

    xiaomi13: dolby: Override HDR types for dolby vision
    Stock display HAL doesn't enable dolby vision support,
    instead miui uses a custom surfaceflinger lib for the
    same. Since we won't be using that, simply override
    supported HDR types at boot.
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- dolby/src/com/xiaomi/dolby/BootCompletedReceiver.java
+++ dolby/src/com/xiaomi/dolby/BootCompletedReceiver.java
@@ -9,7 +9,11 @@ package com.xiaomi.dolby;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.display.DisplayManager;
+import android.os.IBinder;
 import android.util.Log;
+import android.view.Display;
+import android.view.Display.HdrCapabilities;
 
 import com.xiaomi.dolby.DolbyUtils;
 
@@ -33,10 +37,19 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static void onLockedBootCompleted(Context context) {
         // Services that don't require reading from data.
+        overrideHdrTypes(context);
     }
 
     private static void onBootCompleted(Context context) {
         // Data is now accessible (user has just unlocked).
         DolbyUtils.getInstance(context).onBootCompleted();
+    }
+
+    private static void overrideHdrTypes(Context context) {
+        // Override HDR types to enable Dolby Vision
+        final DisplayManager dm = context.getSystemService(DisplayManager.class);
+        dm.overrideHdrTypes(Display.DEFAULT_DISPLAY, new int[]{
+                HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
+                HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
     }
 }
