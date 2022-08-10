d3bb73ae5811953e88620a174f6d3028661f0d51
Arian <arian.kulmer@web.de>
2022-08-10 18:09:53 +0200

    xiaomi13: Build fwk-detect from source
    Change-Id: I2915b0a9a10cade7c42df54cf3c7e48157d938e9

--- device.mk
+++ device.mk
@@ -329,6 +329,9 @@ PRODUCT_PACKAGES += \
     libjson
 
 # QTI
+PRODUCT_PACKAGES += \
+    libvndfwk_detect_jni.qti_vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
