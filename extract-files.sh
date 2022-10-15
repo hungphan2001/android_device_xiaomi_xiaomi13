7080530e0b93efcffaaf7d25a6bc278410eb412a
LuK1337 <priv.luk@gmail.com>
2022-10-15 23:05:18 +0200

    xiaomi13: Prevent adding same dependency twice in blob_fixup
    Change-Id: Idedeb51e027a09f64a29ac67a3a2f2a0594213f6
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -71,7 +71,7 @@ function blob_fixup() {
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
             ;;
         vendor/bin/hw/android.hardware.security.keymint-service-qti | vendor/lib64/libqtikeymint.so)
-            "${PATCHELF}" --add-needed android.hardware.security.rkp-V3-ndk.so "${2}"
+            grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || ${PATCHELF} --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
             ;;
         vendor/bin/hw/dolbycodec2 | vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service | vendor/bin/hw/vendor.dolby.media.c2@1.0-service | vendor/lib64/hw/audio.primary.kalama.so)
             "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
