009e8b6fc9aabcfe6fb8af6b91465bb6f6917b11
LuK1337 <priv.luk@gmail.com>
2023-10-21 16:34:40 +0200

    xiaomi13: Build android.frameworks.sensorservice@1.0.vendor
    Required by mm-pp-deps.
    
    Change-Id: Iaf821dcbb07d7820f2fc3ff5ca9cca5e973360f2

--- device.mk
+++ device.mk
@@ -326,6 +326,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.1-service.xiaomi-multihal \
     libsensorndkbridge \
     sensors.xiaomi
