cc04043749b55f2ca2e3f0af41c5f9801fae4b5d
Michael Bestas <mkbestas@lineageos.org>
2023-11-22 19:21:47 +0200

    xiaomi13: Build recovery QTI health AIDL service
    We have a recovery partition.
    
    Change-Id: I10841b26feea3c214349a20b2eedec74924f993a

--- device.mk
+++ device.mk
@@ -176,7 +176,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.qti
+    android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery
 
 # Hotword Enrollement
 PRODUCT_COPY_FILES += \
