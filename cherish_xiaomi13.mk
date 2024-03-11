526e2bc588355f5e86fe669649b522ea26511393
Hung Phan <phandinhhungvp2001@gmail.com>
2024-03-11 14:38:03 +0000

    xiaomi13:bring cherish
    Signed-off-by: Hung Phan <phandinhhungvp2001@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- cherish_xiaomi13.mk
+++ cherish_xiaomi13.mk
@@ -2,20 +2,26 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common cherish stuff.
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from Xiaomi13 device
 $(call inherit-product, device/xiaomi/xiaomi13/device.mk)
-
-# Inherit from Gapps
-$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := xiaomi13
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := xiaomi13
-PRODUCT_NAME := lineage_xiaomi13
+PRODUCT_NAME := cherish_xiaomi13
+
+# Cherish
+CHERISH_BUILD_TYPE := OFFICIAL
+CHERISH_MAINTAINER := hungphan2001
+CHERISH_CHIPSET := kalama
+CHERISH_BATTERY := 4500mAh
+CHERISH_DISPLAY := 1080x2400
+
+EXTRA_UDFPS_ANIMATIONS := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
