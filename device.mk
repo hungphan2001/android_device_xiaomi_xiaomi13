4aab8bbc65c296cb09460f5b67dc78af6b5460bc
nguyenhung9xdev2024 <143480642+nguyenhung9xdev2024@users.noreply.github.com>
2024-03-29 11:09:00 +0700

    xiaomi13: switch health qti

--- device.mk
+++ device.mk
@@ -176,10 +176,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health-service.xiaomi \
-    android.hardware.health-service.xiaomi_recovery \
-    android.hardware.health@1.0.vendor \
-    android.hardware.health@2.1.vendor
+    android.hardware.health-service.qti
 
 # Hotword Enrollement
 PRODUCT_COPY_FILES += \
