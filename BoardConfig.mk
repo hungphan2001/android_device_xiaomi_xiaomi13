29771b5e953c49d7cedb690ec174e1f3e0100810
nguyenhung9xdev2024 <143480642+nguyenhung9xdev2024@users.noreply.github.com>
2024-03-31 07:04:18 +0700

    xiaomi13: Add system.prop

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -202,6 +202,7 @@ SOONG_CONFIG_XIAOMI_POWERSHARE_WIRELESS_TX_ENABLE_PATH := /sys/class/qcom-batter
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/configs/properties/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/properties/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/system.prop
 TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/properties/system_ext.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
 
