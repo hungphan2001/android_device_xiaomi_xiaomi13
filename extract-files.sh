fb5f867e0fc8a5273e57d0ede11f5e0e59085c88
Michael Bestas <mkbestas@lineageos.org>
2024-04-02 21:39:06 +0200

    xiaomi13: extract-files: Add firmware extraction support
    Change-Id: Ibace60458f9713380d7286dc0abdb92807d23843
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -91,9 +91,17 @@ function blob_fixup() {
     esac
 }
 
+function prepare_firmware() {
+    :
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+if [ -z "${SECTION}" ]; then
+    extract_firmware "${MY_DIR}/proprietary-firmware.txt" "${SRC}"
+fi
 
 "${MY_DIR}/setup-makefiles.sh"
