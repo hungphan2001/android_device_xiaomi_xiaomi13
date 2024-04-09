9bff338c35a6f861ce4fe91f69ef1968da95a208
Arian <arian.kulmer@web.de>
2024-04-10 00:20:48 +0200

    xiaomi13: udfps: Only disable fod_status until suspend
    Change-Id: If4d9084cbcc8670c7636fddd143807cb6b067f22
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -35,6 +35,7 @@
 
 #define FOD_STATUS_OFF 0
 #define FOD_STATUS_ON 1
+#define FOD_STATUS_OFF_UNTIL_SUSPEND 3
 
 #define TOUCH_DEV_PATH "/dev/xiaomi-touch"
 #define TOUCH_MAGIC 'T'
@@ -237,7 +238,7 @@ class XiaomiSm8550UdfpsHander : public UdfpsHandler {
             req.local_hbm_value = LHBM_TARGET_BRIGHTNESS_OFF_FINGER_UP;
             ioctl(disp_fd_.get(), MI_DISP_IOCTL_SET_LOCAL_HBM, &req);
             if (!enrolling) {
-                setFodStatus(FOD_STATUS_OFF);
+                setFodStatus(FOD_STATUS_OFF_UNTIL_SUSPEND);
             }
         } else if (!isFpcFod && vendorCode == 21) {
             setFodStatus(FOD_STATUS_ON);
@@ -249,6 +250,8 @@ class XiaomiSm8550UdfpsHander : public UdfpsHandler {
     void cancel() {
         LOG(DEBUG) << __func__;
         enrolling = false;
+
+        setFodStatus(FOD_STATUS_OFF_UNTIL_SUSPEND);
     }
 
     void preEnroll() {
@@ -265,7 +268,7 @@ class XiaomiSm8550UdfpsHander : public UdfpsHandler {
         LOG(INFO) << __func__;
         enrolling = false;
 
-        setFodStatus(FOD_STATUS_OFF);
+        setFodStatus(FOD_STATUS_OFF_UNTIL_SUSPEND);
     }
 
   private:
