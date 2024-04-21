be37d6bb7adb905a7611bd1c808878fde9d68b1e
nguyenhung9xdev2024 <143480642+nguyenhung9xdev2024@users.noreply.github.com>
2024-04-21 16:58:55 +0700

    xiaomi13: build Servicetracker hal
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- device.mk
+++ device.mk
@@ -499,6 +499,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+# Servicetracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.2.vendor
+
 # Telephony
 PRODUCT_PACKAGES += \
     extphonelib \
