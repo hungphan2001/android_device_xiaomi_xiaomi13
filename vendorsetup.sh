2d7cb0d3bbc702b480c613a48f4612583998c5a9
nguyenhung9xdev2024 <143480642+nguyenhung9xdev2024@users.noreply.github.com>
2024-04-27 09:14:37 +0700

    xiaomi13: fix cache build
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,8 +2,9 @@
 
 # Configure ccache
 export USE_CCACHE=1
-export CCACHE_EXEC=$(which ccache)
-export CCACHE_DIR=$HOME/.ccache
+export CCACHE_EXEC=/usr/bin/ccache
+ccache -M 50G
+croot
 
 # Restrict high mem jobs to 1
 export NINJA_HIGHMEM_NUM_JOBS=1
